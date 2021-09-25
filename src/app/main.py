b8f6182eebdb0d7b706837cdee72ab709884421e
lucapitzoi <luca.pitzoi@sindria.org>
2021-09-25 04:32:46 +0200

    Added base create azure logic

--- src/app/main.py
+++ src/app/main.py
@@ -4,8 +4,13 @@ import os
 import subprocess
 import threading
 import concurrent.futures
+import uuid
+from hashlib import sha256
 
 import helpers
+
+DEPLOY_TEMPLATE = '/var/www/app/templates/deploy.template.ps1'
+BUILD_DIRECTORY = '.build'
 
 # # Pycharm pydevd
 # PYCHARM_PYDEVD_ENABLED = int(os.getenv('PYCHARM_PYDEVD_ENABLED'))
@@ -18,16 +23,33 @@ import helpers
 #     pydevd_pycharm.settrace(PYCHARM_PYDEVD_HOST, port=PYCHARM_PYDEVD_PORT, stdoutToServer=True, stderrToServer=True)
 
 
-# # Create lightsail immutable
-# def create_lightsail(immutable):
-#     subprocess.call(['aws', 'lightsail','create-instances-from-snapshot', '--instance-snapshot-name', immutable['refer'], '--instance-names', immutable['name'], '--availability-zone', immutable['zone'], '--bundle-id', immutable['bundle']])
+# Build unique workspace - return string
+def build_workspace(name):
+    stringId = uuid.uuid4()
+    salted_string = str(stringId) + str(name)
+    hash_result = sha256(str(salted_string).encode())
+    return hash_result.hexdigest()
 
 # Create azure immutable
 def create_azure(immutable):
-    pass
 
+    if not os.path.exists(BUILD_DIRECTORY):
+        os.makedirs(BUILD_DIRECTORY)
 
-# Multi process azure immutable
+    workspace = build_workspace(immutable['name'])
+
+    if not os.path.exists(BUILD_DIRECTORY + '/' + workspace):
+        os.makedirs(BUILD_DIRECTORY + '/' + workspace)
+
+    subprocess.call('cp '+DEPLOY_TEMPLATE+' '+BUILD_DIRECTORY + '/' + workspace+'/deploy.ps1', shell=True)
+
+    # TODO: build deploy.ps1 with values
+
+    # Execute powershell script
+    stdout = subprocess.check_output(['pwsh', BUILD_DIRECTORY + '/' + workspace + '/deploy.ps1'], universal_newlines=True)
+    print(stdout)
+
+# Multi process azure immutables
 def process_azure(azure_immutables):
     with concurrent.futures.ProcessPoolExecutor() as executor:
         results = [executor.submit(create_azure, immutable) for immutable in azure_immutables]
